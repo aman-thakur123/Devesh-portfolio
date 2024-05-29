74b517cac04125e7645265408e1a53d2157f21c0
unknown <aman.100100@gmail.com>
2024-05-29 21:51:13 +0530

    error fixed for key props

--- src/app/case-studies/Twogridsection.tsx
+++ src/app/case-studies/Twogridsection.tsx
@@ -18,9 +18,9 @@ export default function Twogridsection({ griddata }: TwogridsectionProps) {
     return (
         <>
             {griddata.map((item, index) => (
-                <section className={`${index%2 ===0 ? 'odd-grid' : 'even-grid'}`}>
+                <section key={index} className={`${index%2 ===0 ? 'odd-grid' : 'even-grid'}`}>
                     <div className="container mx-auto">
-                        <div key={index} className="grid grid-cols-2 gap-4 py-32">
+                        <div className="grid grid-cols-2 gap-4 py-32">
                             <div className="image-section">
                                 <Image src={item.pathurl} alt={item.altname} />
                             </div>
